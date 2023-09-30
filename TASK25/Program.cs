22b294bb809b3248f36cbdf2bc64d7be27ddd6a9
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-30 14:27:36 +0300

    added Task25

---
+++ TASK25/Program.cs
@@ -0,0 +1,26 @@
+﻿//using System.Globalization;
+
+void InputNum(ref double num1, ref double num2)
+{
+    Console.WriteLine("Введите число для возведения в степень 'N'");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень 'N' в которую хотите"
+    + "возвести число");
+    num2 = Convert.ToInt32(Console.ReadLine());
+}
+
+double Exponentiation(double num1, double num2) 
+{
+    double result = 0;
+    InputNum(ref num1, ref num2);
+    for (int i = 1; i < num2; i++)
+    {
+        result += num1 * num1; 
+    }
+    return result;
+}
+
+double num1 = 0;
+double num2 = 0;
+
+Console.WriteLine(Exponentiation(num1, num2));
