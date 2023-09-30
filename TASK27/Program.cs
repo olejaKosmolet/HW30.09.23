ed084dedd7f19eac375a5d886bc65b049e4040ce
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-30 15:23:21 +0300

    Added task2 # Please enter the commit message for your changes. Lines starting

---
+++ TASK27/Program.cs
@@ -0,0 +1,21 @@
+﻿int SumOfNumber(int number)
+{
+    int result = 0;    
+    while(number > 0)
+    {
+        result += number% 10;
+        number/= 10;
+    }
+    return result;
+}  
+
+int InputNum (ref int number)
+{
+    Console.Write("Введите целое число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = 0;
+int num = SumOfNumber(InputNum(ref number));
+Console.WriteLine(num);
