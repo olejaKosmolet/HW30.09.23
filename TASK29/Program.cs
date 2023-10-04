170820f544d60b32dfd4dcbbd0b62ee02be81000
oleja_kosmolet <olegermolaev45@gmail.com>
2023-10-04 19:02:26 +0300

    added tasks

---
+++ TASK29/Program.cs
@@ -0,0 +1,29 @@
+﻿// void ArrrayOutput(int amount)
+// {
+//     int[] array = new int[amount];
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     Console.Write($"[{string.Join(",", array)}]");
+// }
+
+// int amount = Convert.ToInt32(Console.ReadLine());
+
+// ArrrayOutput(amount);
+
+void ArrrayOutput(int amount)
+{
+    int[] array = new int[amount];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    Console.Write($"[{string.Join(",", array)}]");
+}
+
+int input = Convert.ToInt32(Console.ReadLine());
+
+ArrrayOutput(input);
